9156f8566369bf36927270358ebebf9ea1a5fbfa
TexasJeff75 <texasjeff75@gmail.com>
2025-09-12 15:46:08 -0500

    Add scroll to top functionality for all page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
@@ -21,6 +22,7 @@ import './index.css';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-gray-50">
         <Header />
         <main>
