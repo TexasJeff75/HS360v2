94a6a8683d66fa752e90c0be98f06fd70ced3213
TexasJeff75 <texasjeff75@gmail.com>
2025-09-12 19:21:35 -0500

    Replace bright gradient with dark gradient on patients page

--- src/pages/ForPatients.tsx
+++ src/pages/ForPatients.tsx
@@ -170,7 +170,7 @@ const ForPatients = () => {
         </section>
 
         {/* How It Works */}
-        <section className="py-24 bg-gradient-secondary text-white">
+        <section className="py-24 bg-gradient-dark text-white">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <motion.div
               initial={{ opacity: 0, y: 20 }}
