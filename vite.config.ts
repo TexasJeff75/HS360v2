303540a67241890649c5583c585ccaacd29eae6e
TexasJeff75 <texasjeff75@gmail.com>
2025-09-20 13:27:10 -0500

    Add Cross-Origin headers to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'unsafe-none',
+      'Cross-Origin-Resource-Policy': 'cross-origin',
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
